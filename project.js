4dd6de3abfff3bfa25ef0dbf09e4681c63817758
asim04-byte <mdasim200204@gmail.com>
2024-10-21 22:55:54 +0530

    first commit

--- project.js
+++ project.js
@@ -4,17 +4,12 @@ const exp_amt = document.querySelector("#exp-amt");
 const trans = document.querySelector("#trans");
 const form = document.querySelector("#form");
 const description = document.querySelector("#desc");
-const amount = document.querySelector("#amount");
-
-const dummyData = [
-    {id:1,description:"flower",amount:-20},
-    {id:2,description:"salary",amount:35000},
-    {id:3,description:"book",amount:-10},
-    {id:4,description:"camera",amount:-150},
-    {id:5,description:"petrol",amount:-120},
-];
-
-let transactions = dummyData;
+// const amount = document.getElementById("amount");
+const amount = document.getElementById('amounts')
+console.log("am",amount)
+console.log("amv",amount.value)
+const localStoragTrans = JSON.parse(localStorage.getItem("trans"));
+let transactions = localStorage.getItem("trans") !==null? localStoragTrans:[];
 
 function loadTransactionDetails(transaction){
     const sign = transaction.amount < 0 ?"-":"+";
@@ -32,6 +27,7 @@ function removeTrans(id){
         {
             transactions = transactions.filter((transaction)=>transaction.id != id);
             confiq();
+            updateLocalStorage();
     } else{
         return;
     }
@@ -41,10 +37,8 @@ function updateAmount(){
     // console.log(amounts)
     const total = amounts.reduce((acc,item)=>(acc+=item),0).toFixed(2);
     balance.innerHTML =`₹ ${ total }`;
-
     const income = amounts.filter((item)=>item>0).reduce((acc,item)=>(acc+=item),0).toFixed(2);
     inc_amt.innerHTML =`₹ ${ income }`;
-
     const expense = amounts.filter((item)=>item<0).reduce((acc,item)=>(acc+=item),0).toFixed(2);
     exp_amt.innerHTML =`₹ ${ Math.abs(expense) }`;
 }
@@ -52,32 +46,39 @@ function confiq() {
     trans.innerHTML = "";
     transactions.forEach(loadTransactionDetails);
     updateAmount();
-
 }
-
 function addTransaction(e){
     e.preventDefault();
-    if (description.value.trim()=="" || amount.value.trim()==""){
+    if (description.value=="" || amount.value==""){
+        console.log(1)
         alert("Please Enter Description and amount");
     } else {
+        console.log('done')
         const transaction = {
-            id:25121,
+            id: uniqueId(),
             description:description.value,
             amount: +amount.value,
+            
         };
+        console.log("description",description.value)
+        console.log("amount",amount.value)
         transactions.push(transaction);
         loadTransactionDetails(transaction);
         description.value = "";
         amount.value = "";
         updateAmount();
-        
-
     }
 }
-
+function uniqueId(){
+    return Math.floor(Math.random()*1000000)
+    
+}
 
 form.addEventListener("submit", addTransaction);
 
 window.addEventListener("load",function() {
     confiq();
 });
+function updateLocalStorage(){
+    localStorage.setItem("trans",JSON.stringify(transactions))
+}
